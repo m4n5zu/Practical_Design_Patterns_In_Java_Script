084abfd81db3b199ee70cd85bef8f0941e2f97d7
Krzysztof Mansz <m4n5zu@MacBook-Air-Krzysztof.local>
2017-06-04 13:53:52 +0200

    adding first file

---
+++ task.js
@@ -0,0 +1,7 @@
+var task = {};
+//the same result as above var task = Object.create(Object.prototype);
+task.title = 'My task';
+task.description = 'My description';
+task.toString = function
+
+console.log(task.title);
